59166fd760d2d445f4d4ab7510dca9783bb098ed
Théo Migeat <44432694+STM3900@users.noreply.github.com>
2021-11-15 10:46:42 +0100

    add json and file support

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Import des bibliothèques
 from PIL import Image
 import pprint
+import json
 
 # Variable d'initialisation
 img = Image.open('img.png')  # Chemin vers l'image étant dans le dossier
@@ -45,3 +46,15 @@ for key, value in colorTab.items():
 
 # Affichage en console du dictionnaire
 pprint.pprint(finalDict)
+
+# Écriture du dictionnaire dans un fichier texte
+f = open("data.txt", "w+")
+
+for key, value in finalDict.items():
+    f.write('%s:%s\n\n' % (key, value))
+
+f.close()
+
+# Écriture du dictionnaire dans un fichier json
+with open('data.json', 'w') as convert_file:
+    convert_file.write("[" + json.dumps(finalDict) + "]")
